f67ae53ed469c1df4b11c6831fc36d3fe493a575
Richard <richard.esclima@gmail.com>
2025-01-18 18:30:13 -0300

    feat: adiciona função  para leitura de comandos via serial

--- controle-de-pinos-GPIO.c
+++ controle-de-pinos-GPIO.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include <string.h>
 
-
+// FUNÇÃO PARA LER COMANDOS VIA SERIAL
+int read_serial_command(char *command, size_t size);
 
 int main()
 {
     stdio_init_all();
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    char command[20];
+    while (true)
+    {
+        if (read_serial_command(command, sizeof(command)))
+        {
+            printf("comando lido: %s\n", command);
+        }
     }
+}
+
+// FUNÇÃO PARA LER COMANDOS VIA SERIAL-> retorna 1 se tiver alguma leitura e 0 caso contrário
+int read_serial_command(char *command, size_t size)
+{
+    static int index = 0;
+
+    int ch = getchar_timeout_us(10000); // período máximo de 10 milissegundos
+    if (ch != PICO_ERROR_TIMEOUT)
+    {
+        if (ch == '\n' || ch == '\r')
+        {
+            if (index > 0)
+            {
+                command[index] = '\0';
+                index = 0; // após ler o comando volta reseta o índice
+                return 1;  // indica que houve leitura
+            }
+        }
+        else if (index < size - 1)
+        {
+            command[index++] = (char)ch;
+        }
+    }
+    return 0; // indica que não houve leitura
 }
